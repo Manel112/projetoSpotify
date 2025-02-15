4da1976a93d274c6547a78b62cea45bb10df2b01
Emanuel Lucas Alves de Souza <emanuel0206@gmail.com>
2025-02-15 14:16:47 -0300

    ajustes para o deploy

--- back_end/api/server.js
+++ back_end/api/server.js
@@ -4,22 +4,30 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Olá, Mundo, atualizou? E agora?");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front_end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front_end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
